c2bc420dfba120c5163cd03728b9d2336e9d687f
Aditya Kota <kota_aditya@hotmail.com>
2024-06-19 20:08:14 -0400

    First full working version

--- GameOfLife/Models/GameOfLife.Models.cs
+++ GameOfLife/Models/GameOfLife.Models.cs
@@ -1,10 +1,19 @@
-﻿namespace GameOfLife.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameOfLife.Models
 {
     public class GameOfLifeBoard
     {
-        public string Id { get; set; }
+        [Required]
+        [MinLength(1, ErrorMessage = "The board must have at least one row.")]
         public int[][] State { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "The number of rows must be at least 1.")]
         public int Rows { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "The number of columns must be at least 1.")]
         public int Columns { get; set; }
+
+        public string? Id { get; set; }
     }
 }
